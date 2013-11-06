9dc8abf85518a7caad712b40b97814c69d6b760d
Jappie Klooster <jappieklooster@hotmail.com>
2013-11-06 22:03:29 +0100

    user friendlyish input accepted

--- execute.py
+++ execute.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python
-
 import sys
 
-for x in range(0, int(sys.argv[1])):
+# read user input (how often to execute the code)
+default = 1
+if len(sys.argv) > 1:
+	loops = int(sys.argv[1])
+	loops = loops if loops > 1 else default
+else:
+	loops = default
+
+for x in range(0, loops):
 	print ('Name:' + sys.argv[0])
