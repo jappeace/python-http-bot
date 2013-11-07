de5aa6645c3bf6dfc1ae8a1a429a76fa371ed956
Jappie Klooster <jappieklooster@hotmail.com>
2013-11-07 22:06:05 +0100

    figured out how privete fields work, xD and mapped the first form

--- bot.py
+++ bot.py
@@ -1,11 +1,21 @@
-from requests import get, codes
+from requests import get, codes, post
 class hyperTexter:
-	def __init__(self):
-		self.mail = str('http://mailinator.com')
+	__mail = 'http://mailinator.com'
+	__vote = 'http://restaurantverkiezing.nl'
 	def vote(self, name):
 		print ('creating inbox for ' + name)
-		if get(self.mail + '/inbox.jsp',params={'to' : name }).status_code != codes.ok:
+		if get(self.__mail + '/inbox.jsp',params={'to' : name }).status_code != codes.ok:
 			return False
-		print('succes')
+		rep = post(self.__vote + '/state8/flow20551',
+				params = {
+					'form_id' : 'beoordelen',
+					'mField2' : '10',
+					'mField3' : '10',
+					'mField6' : '10',
+					'mField7' : '10',
+					'mField40' : '',
+					'mField41' : ''
+				})
+		print(rep.read())
 		return True
 
